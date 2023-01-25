9aeca196d7141ae5a45033de1079114d693dce12
Alejandro Serrano Perez <alejandro.serrano@alumnos.upm.es>
2023-01-25 18:18:27 +0100

    Fichero csv

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,9 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Equipo {
     String nombre;
     List<String> trofeosEquipo;
     Racha ultimosJugados;
     List<Jugador> plantilla;
+    Entrenador entrenador;
 
+
+    public Equipo (String rutaFichero){
+        try{
+            Scanner sc = new Scanner(new File(rutaFichero));
+            sc.useDelimiter(",");
+            plantilla = new ArrayList<Jugador>();
+            while (sc.hasNext()){
+                switch (sc.next()){
+                    case "Portero": Portero provisionalPort = new Portero(); plantilla.add(provisionalPort);break;
+                    case "Defensa": Defensa provisionalDef = new Defensa(); plantilla.add(provisionalDef);break;
+                    case "Delantero": Delantero provisionalDel = new Delantero(); plantilla.add(provisionalDel);break;
+                    case "Entrenador": Entrenador provisionalEntr = new Entrenador(); this.entrenador = provisionalEntr;break;
+                }
+
+            }
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
