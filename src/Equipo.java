c3b9f1d697b32fd12d51f0f376d684f37ae319e4
Alejandro Serrano Perez <alejandro.serrano@alumnos.upm.es>
2023-01-27 11:45:48 +0100

    Leer documentos funcional

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,5 +1,10 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,7 +16,7 @@ public class Equipo {
     List<Jugador> plantilla;
     Entrenador entrenador;
 
-
+    /*
     public Equipo (String rutaFichero){
         try{
             Scanner sc = new Scanner(new File(rutaFichero));
@@ -30,6 +35,30 @@ public class Equipo {
             throw new RuntimeException(e);
         }
 
+    }
+    */
+    public Equipo (String ruta){
+        this.plantilla = new ArrayList<Jugador>();
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(ruta));
+            String[] fila = null;
+            while ((fila = csvReader.readNext()) != null){
+                switch (fila[0]){
+                     case "Portero": Portero provisionalPort = new Portero(); plantilla.add(provisionalPort);break;
+                     case "Defensa": Defensa provisionalDef = new Defensa(); plantilla.add(provisionalDef);break;
+                     case "Mediocentro": Mediocentro provisionalMed = new Mediocentro(); plantilla.add(provisionalMed);break;
+                     case "Delantero": Delantero provisionalDel = new Delantero(fila); plantilla.add(provisionalDel);break;
+                     case "Entrenador":this.entrenador = new Entrenador();break;
+             }
+            }
+        } catch (CsvValidationException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void replaceSpaces (String[] string){
+        for (String hueco: string) {
+            hueco.replace(" ", "");
+        }
     }
     public void mostrar(){
         for(Jugador jugador : plantilla){
